d9ecb6212bc20b0d1371fed528d379a7c0258103
Cro Joe <christianrosales777@gmail.com>
2025-08-12 16:16:27 -0400

    Recreated a logger to catch requests

---
+++ controller/volunteersController.js
@@ -0,0 +1,14 @@
+const Volunteer = require('../model/Volunteer');
+
+const getVolunteerAvailability = async (req, res) =>{
+    if(!req?.param?.id) return res.status(400).json({'message': 'Volunteer ID required'})
+        const volunteer = await Volunteer.findOne({_id: req.params.id}).exec();
+    if(!volunteer){
+        return res.status(204).json({'message': `No volunteer matches the ID${req.body.id}`})
+    }
+    res.json(volunteer);
+}
+
+module.exports = {
+    getVolunteerAvailability
+}
