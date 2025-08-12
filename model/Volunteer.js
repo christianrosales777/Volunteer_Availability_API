94868af4dba92059b1c6269b803595103685fcc9
Cro Joe <christianrosales777@gmail.com>
2025-08-12 09:58:24 -0400

    Created schema for Volunteer

---
+++ model/Volunteer.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const volunteerSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    campus: String,
+    availability: [String]
+})
