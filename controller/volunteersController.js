2c8d3b3df3f2388a3ef593fdbd726fa0e1565964
Cro Joe <christianrosales777@gmail.com>
2025-08-13 00:49:28 -0400

    Added put updateVolunteerAvailability to catch invalid indexes and update mongo

--- controller/volunteersController.js
+++ controller/volunteersController.js
@@ -21,6 +21,28 @@ let avails = [];
     return avails;
 }
 
+ const updateVolunteerAvailability = async (req, res) =>{
+    if(!req?.body?.id){
+        return res.status(400).json({'message': 'ID parameter is required.'});
+    }
+
+    if(!req.body.i){
+        return res.status(400).json({'message': 'i: index parameter is required.'})
+    }
+
+    const volunteer = await Volunteer.findOne({_id: req.body.id}).exec();
+    if (!volunteer){
+        return res.status(204).json({'message': `No volunteer matches the ID ${req.body.id}`});
+    }
+    if(req.body.i > volunteer.availability.length - 1 || req.body.i < 0){
+        return res.status(400).json({'message': 'i: index parameter must be within bounds of current availability.'})
+    }
+    if(req.body?.from) volunteer.availability[req.body.i].from = req.body.from;
+    if(req.body?.to) volunteer.availability[req.body.i].to = req.body.to;
+    const result = await volunteer.save();
+    res.json(result);
+    };
+
 const getVolunteerAvailability = async (req, res) => {
     if (!req?.params?.id || req.params.id.length !== dbIDLength) return res.status(400).json({ 'message': 'Volunteer ID required' })
     const volunteer = await Volunteer.findOne({ _id: req.params.id }).exec();
@@ -37,6 +59,7 @@ const getVolunteerAvailability = async (req, res) => {
 
 
 module.exports = {
+    updateVolunteerAvailability,
     getVolunteerAvailability
 }
 
