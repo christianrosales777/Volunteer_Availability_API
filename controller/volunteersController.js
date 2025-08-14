fe0b07add1b24b4c3a19ddbfa60a2a8f20aca7b0
Cro Joe <christianrosales777@gmail.com>
2025-08-14 11:06:35 -0400

    Fixed error/typo inside of getVolunteerAvailability, was suppose to be retrieving params in get and not body

--- controller/volunteersController.js
+++ controller/volunteersController.js
@@ -70,7 +70,7 @@ const getVolunteerAvailability = async (req, res) => {
     if (!req?.params?.id || req.params.id.length !== dbIDLength) return res.status(400).json({ 'message': 'Volunteer ID required' })
     const volunteer = await Volunteer.findOne({ _id: req.params.id }).exec();
     if (!volunteer) {
-        return res.status(204).json({ 'message': `No volunteer matches the ID${req.body.id}` })
+        return res.status(204).json({ 'message': `No volunteer matches the ID${req.params.id}` })
     }
     res.json(volunteerSlots(volunteer));
 }
