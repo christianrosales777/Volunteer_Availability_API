183be30aba41c0f599577a84d20b0392b7935123
Cro Joe <christianrosales777@gmail.com>
2025-08-14 04:36:35 -0400

    Added swagger-api documentation to allow easier follow through of what is being achieved with endpoints

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yaml');
 const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
+const fs = require('fs')
 const PORT = process.env.PORT || 4567;
+
+const file = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerDocument = YAML.parse(file);
 
 connectDB();
 
@@ -14,6 +19,7 @@ app.use(logger);
 
 app.use(express.json());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/volunteers', require('./routes/api/volunteer'));
 app.use('/campuses', require('./routes/api/campus'))
 
